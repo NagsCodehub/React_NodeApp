aa9229ce7ad4f4292ff2c19c938478352bbb5947
NAGA KARUMANCHI <nagamallik.net@gmail.com>
2016-12-22 17:17:38 -0500

    react component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const color = Math.random() > 0.5 ? 'green' : 'red';
+// const color = Math.random() > 0.5 ? 'green' : 'red';
 
-ReactDOM.render(
-   // React.createElement('h2', null, 'Hello React'),
-   <h2 style ={{color}} className = "text-center"> 
-   Here is JSX == { Math.random ()}
-   </h2>,   
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//    // React.createElement('h2', null, 'Hello React'),
+//    <h2 style ={{color}} className = "text-center"> 
+//    Here is JSX == { Math.random ()}
+//    </h2>,   
+//     document.getElementById('root')
+// );
+
+const App = (props) =>
+{
+    return 
+    (
+        <h2 className="text-center">
+         {  props.headerMessage }
+        </h2>
+    );
+ };
+
+App.propTypes={
+    headerMessage:React.PropTypes.String.isRequired
+};
+
+// App.defaultProps={
+//     headerMessage:"Here we go with props"
+// };
+
+ ReactDOM.render(
+     <App headerMessage="Component Props" />,
+     document.getElementById('root')
+ );
